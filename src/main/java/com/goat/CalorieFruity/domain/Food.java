b6a902effaea21e323aea34dccb6299dd98b6834
Goat <spider_child2001@yahoo.com>
2021-07-06 18:05:59 +0300

    Database configuration, class update and homepage update

--- src/main/java/com/goat/CalorieFruity/domain/Food.java
+++ src/main/java/com/goat/CalorieFruity/domain/Food.java
@@ -2,9 +2,103 @@ package com.goat.CalorieFruity.domain;
 
 import org.springframework.stereotype.Component;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Component
 public class Food {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ID;
     private String name;
+    private String foodGroup;
+    private int calories;
+    private double fat;
+    private double protein;
+    private double carbohydrate;
+    private double sugars;
+    private double fiber ;
+    private int cholesterol;
+    private double saturatedFats;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoodGroup() {
+        return foodGroup;
+    }
+
+    public void setFoodGroup(String foodGroup) {
+        this.foodGroup = foodGroup;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(double carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public double getSugars() {
+        return sugars;
+    }
+
+    public void setSugars(double sugars) {
+        this.sugars = sugars;
+    }
+
+    public double getFiber() {
+        return fiber;
+    }
+
+    public void setFiber(double fiber) {
+        this.fiber = fiber;
+    }
+
+    public int getCholesterol() {
+        return cholesterol;
+    }
+
+    public void setCholesterol(int cholesterol) {
+        this.cholesterol = cholesterol;
+    }
+
+    public double getSaturatedFats() {
+        return saturatedFats;
+    }
+
+    public void setSaturatedFats(double saturatedFats) {
+        this.saturatedFats = saturatedFats;
+    }
 }
